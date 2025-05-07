d3f6376db8e0999ea4a1a8b9d29438441db20d74
youngs <lixiang@popicorns.com>
2025-05-07 20:03:19 +0800

    feat：按需加载

--- admin_client/src/App.js
+++ admin_client/src/App.js
@@ -1,11 +1,13 @@
-import React, { Component } from 'react'
-
+import React, { Component } from "react";
+import { Button } from "antd";
+import "./App.less";
 export default class App extends Component {
   render() {
     return (
       <div>
         <h1>Hello World</h1>
+        <Button type="primary">Click me</Button>
       </div>
-    )
+    );
   }
 }
