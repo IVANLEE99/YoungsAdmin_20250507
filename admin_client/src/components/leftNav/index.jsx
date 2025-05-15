a5de67fd94efe9dab238aea41a5f626d7b05ae85
youngs <lixiang@popicorns.com>
2025-05-15 20:07:19 +0800

    feat:menuConfig

--- admin_client/src/components/leftNav/index.jsx
+++ admin_client/src/components/leftNav/index.jsx
@@ -1,62 +1,20 @@
 import React, { Component } from "react";
 import "./index.less";
 import { Link } from "react-router-dom";
-import {
-  AppstoreOutlined,
-  ContainerOutlined,
-  DesktopOutlined,
-  MailOutlined,
-  MenuFoldOutlined,
-  MenuUnfoldOutlined,
-  PieChartOutlined,
-} from "@ant-design/icons";
 import { Button, Menu } from "antd";
 import logo from "../../assets/images/logo.png";
-
-const items = [
-  { key: "1", icon: <PieChartOutlined />, label: "首页" },
-  {
-    key: "2",
-    icon: <DesktopOutlined />,
-    label: "商品",
-    children: [
-      { key: "2-1", label: "商品管理" },
-      { key: "2-2", label: "商品分类" },
-      { key: "2-3", label: "商品规格" },
-      { key: "2-4", label: "商品评论" },
-    ],
-  },
-  { key: "3", icon: <ContainerOutlined />, label: "订单" },
-  {
-    key: "4",
-    label: "系统设置",
-    icon: <MailOutlined />,
-    children: [
-      { key: "4-1", label: "Option 5" },
-      { key: "4-2", label: "Option 6" },
-      { key: "4-3", label: "Option 7" },
-      { key: "4-4", label: "Option 8" },
-    ],
-  },
-  {
-    key: "5",
-    label: "Navigation Two",
-    icon: <AppstoreOutlined />,
-    children: [
-      { key: "5-1", label: "Option 9" },
-      { key: "5-2", label: "Option 10" },
-      {
-        key: "5-3",
-        label: "Submenu",
-        children: [
-          { key: "5-3-1", label: "Option 11" },
-          { key: "5-3-2", label: "Option 12" },
-        ],
-      },
-    ],
-  },
-];
+import menuList from "../../config/menuConfig";
 export default class LeftNav extends Component {
+  getItem = (menuList) => {
+    return menuList.map((item) => {
+      return {
+        key: item.key,
+        icon: item.icon,
+        label: item.title,
+        children: item.children ? this.getItem(item.children) : null,
+      };
+    });
+  };
   render() {
     return (
       <div className="left-nav">
@@ -69,7 +27,7 @@ export default class LeftNav extends Component {
           defaultOpenKeys={["sub1"]}
           mode="inline"
           theme="dark"
-          items={items}
+          items={this.getItem(menuList)}
         />
       </div>
     );
---
+++ admin_client/src/config/menuConfig.js
@@ -0,0 +1,70 @@
+import {
+  AppstoreOutlined,
+  ContainerOutlined,
+  DesktopOutlined,
+  MailOutlined,
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  PieChartOutlined,
+  UserOutlined,
+  BarChartOutlined,
+  LineChartOutlined,
+} from "@ant-design/icons";
+const menuList = [
+  {
+    title: "首页",
+    key: "/home",
+    icon: <PieChartOutlined />,
+  },
+  {
+    title: "商品",
+    key: "/products",
+    icon: <ContainerOutlined />,
+    children: [
+      {
+        title: "商品分类",
+        key: "/products/category",
+        icon: <DesktopOutlined />,
+      },
+      {
+        title: "商品管理",
+        key: "/products/product",
+        icon: <ContainerOutlined />,
+      },
+    ],
+  },
+  {
+    title: "用户管理",
+    key: "/users",
+    icon: <UserOutlined />,
+  },
+  {
+    title: "角色管理",
+    key: "/role",
+    icon: <UserOutlined />,
+  },
+  {
+    title: "图表",
+    key: "/charts",
+    icon: <PieChartOutlined />,
+    children: [
+      {
+        title: "柱状图",
+        key: "/charts/bar",
+        icon: <BarChartOutlined />,
+      },
+      {
+        title: "折线图",
+        key: "/charts/line",
+        icon: <LineChartOutlined />,
+      },
+      {
+        title: "饼图",
+        key: "/charts/pie",
+        icon: <PieChartOutlined />,
+      },
+    ],
+  },
+];
+
+export default menuList;
