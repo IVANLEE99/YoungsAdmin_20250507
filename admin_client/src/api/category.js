c4bc6b5ad10515bd9dcab011cb1c3d000c6a43c4
HUANHUAN <yihuan@HUANHUANdeMacBook-Air.local>
2025-05-20 23:09:33 +0800

    feat:分类累接口

---
+++ admin_client/src/api/category.js
@@ -0,0 +1,9 @@
+import { ajax } from "./ajax";
+import { RequestCategory, RequestAddCategory, RequestUpdateCategory } from "./urls/login";
+
+//获取分类列表
+export const getCategoryList = (parentId) => ajax(RequestCategory, { parentId });
+//添加分类
+export const addCategory = ({ categoryName, parentId }) => ajax(RequestAddCategory, { categoryName, parentId }, "POST");
+//更新分类
+export const updateCategory = ({ categoryName, parentId }) => ajax(RequestUpdateCategory, { categoryName, parentId }, "POST");
---
+++ admin_client/src/api/urls/category.js
@@ -0,0 +1,3 @@
+export const RequestCategory = "/manage/category/list";
+export const RequestAddCategory = "/manage/category/add";
+export const RequestUpdateCategory = "/manage/category/update";
