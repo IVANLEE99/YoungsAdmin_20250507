18f9de86217c88e912d4a774422d8e33366fae9f
youngs <lixiang@popicorns.com>
2025-05-21 11:40:28 +0800

    feat:商品分类-获取数据

--- admin_client/src/api/category.js
+++ admin_client/src/api/category.js
@@ -1,9 +1,16 @@
 import { ajax } from "./ajax";
-import { RequestCategory, RequestAddCategory, RequestUpdateCategory } from "./urls/login";
+import {
+  RequestCategory,
+  RequestAddCategory,
+  RequestUpdateCategory,
+} from "./urls/category";
 
 //获取分类列表
-export const getCategoryList = (parentId) => ajax(RequestCategory, { parentId });
+export const getCategoryList = (parentId = "0") =>
+  ajax(RequestCategory, { parentId });
 //添加分类
-export const addCategory = ({ categoryName, parentId }) => ajax(RequestAddCategory, { categoryName, parentId }, "POST");
+export const addCategory = ({ categoryName, parentId }) =>
+  ajax(RequestAddCategory, { categoryName, parentId }, "POST");
 //更新分类
-export const updateCategory = ({ categoryName, parentId }) => ajax(RequestUpdateCategory, { categoryName, parentId }, "POST");
+export const updateCategory = ({ categoryName, parentId }) =>
+  ajax(RequestUpdateCategory, { categoryName, parentId }, "POST");
--- admin_client/src/pages/category/index.jsx
+++ admin_client/src/pages/category/index.jsx
@@ -1,24 +1,25 @@
 import React, { Component } from "react";
-import { Card, Space } from "antd";
+import { Card, Space, message } from "antd";
 import "./index.less";
 import LinkButton from "../../components/linkButton";
 import { PlusOutlined } from "@ant-design/icons";
 import { Table, Button } from "antd";
+import { getCategoryList } from "../../api/category";
 export default class Category extends Component {
   state = {
-    dataSource: [
-      {
-        key: "1",
-        name: "分类名称1",
-        age: 32,
-        address: "西湖区湖底公园1号",
-      },
-      {
-        key: "2",
-        name: "分类名称2",
-        age: 42,
-        address: "西湖区湖底公园1号",
-      },
+    categoryList: [
+      // {
+      //   key: "1",
+      //   name: "分类名称1",
+      //   age: 32,
+      //   address: "西湖区湖底公园1号",
+      // },
+      // {
+      //   key: "2",
+      //   name: "分类名称2",
+      //   age: 42,
+      //   address: "西湖区湖底公园1号",
+      // },
     ],
 
     columns: [
@@ -40,9 +41,28 @@ export default class Category extends Component {
         ),
       },
     ],
+    isLoading: false,
   };
+  getCategoryList = async () => {
+    this.setState({ isLoading: true });
+    const [err, res] = await getCategoryList();
+    this.setState({ isLoading: false });
+    console.log(err, res);
+    if (err) {
+      message.error(err.message);
+    } else {
+      this.setState({
+        categoryList: res.data,
+      });
+    }
+  };
+  // 获取分类列表
+  componentDidMount() {
+    this.getCategoryList();
+  }
 
   render() {
+    const { isLoading } = this.state;
     const title = <h1>一级分类列表</h1>;
     const extra = (
       <LinkButton>
@@ -50,15 +70,16 @@ export default class Category extends Component {
         添加分类
       </LinkButton>
     );
-    const { dataSource, columns } = this.state;
+    const { categoryList, columns } = this.state;
     return (
       <div className="category">
         <Card title={title} extra={extra} style={{ width: "100%" }}>
           <Table
             bordered
             rowKey="key"
-            dataSource={dataSource}
+            dataSource={categoryList}
             columns={columns}
+            loading={isLoading}
           />
         </Card>
       </div>
