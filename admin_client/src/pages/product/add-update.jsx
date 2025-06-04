50115aeaf9d8d791582aa0de77f7d45f57052ace
youngs <lixiang@popicorns.com>
2025-06-04 16:15:21 +0800

    feat:商品分类

--- admin_client/src/pages/product/add-update.jsx
+++ admin_client/src/pages/product/add-update.jsx
@@ -1,16 +1,107 @@
 import React, { Component } from "react";
-import { Card, Form, Input, Button, message, Upload, InputNumber } from "antd";
+import {
+  Card,
+  Form,
+  Input,
+  Button,
+  message,
+  Upload,
+  InputNumber,
+  Cascader,
+} from "antd";
 
 import { UploadOutlined } from "@ant-design/icons";
 import { addProduct } from "../../api/product";
 import LinkButton from "../../components/linkButton";
+import { getCategoryList } from "../../api/category";
 const { TextArea } = Input;
 export default class AddUpdate extends Component {
   static propTypes = {};
+  state = {
+    optionLists: [
+      {
+        value: "zhejiang",
+        label: "Zhejiang",
+        isLeaf: false,
+      },
+      {
+        value: "jiangsu",
+        label: "Jiangsu",
+        isLeaf: false,
+      },
+    ],
+  };
   formRef = React.createRef();
-  componentDidMount() {}
+  getCategoryList = async (parentId = "0") => {
+    const [err, res] = await getCategoryList(parentId);
+    console.log(err, res);
+    if (err) {
+      message.error(err.message || err.msg);
+      return;
+    }
+    if (res.status === 0) {
+      if (parentId === "0") {
+        this.initOptionLists(res.data);
+      } else {
+        return res.data || "";
+      }
+    }
+  };
+  initOptionLists = (data) => {
+    const optionLists = data.map((item) => ({
+      value: item._id,
+      label: item.name,
+      isLeaf: false,
+    }));
+    this.setState({
+      optionLists,
+    });
+  };
+  componentDidMount() {
+    this.getCategoryList();
+  }
   handleSubmit = () => {
     this.formRef.current.submit();
+  };
+  loadData = async (selectedOptions) => {
+    // console.log(selectedOptions);
+    const targetOption = selectedOptions[selectedOptions.length - 1];
+
+    let children = await this.getCategoryList(targetOption.value);
+    if (children && children.length > 0) {
+      targetOption.children = children.map((i) => {
+        return {
+          label: i.name,
+          value: i._id,
+          isLeaf: false,
+        };
+      });
+    } else {
+      targetOption.isLeaf = true;
+    }
+    this.setState({
+      optionLists: [...this.state.optionLists],
+    });
+
+    // // load options lazily
+    // setTimeout(() => {
+    //   targetOption.children = [
+    //     {
+    //       label: `${targetOption.label} Dynamic 1`,
+    //       value: "dynamic1",
+    //     },
+    //     {
+    //       label: `${targetOption.label} Dynamic 2`,
+    //       value: "dynamic2",
+    //     },
+    //   ];
+    //   this.setState({
+    //     optionLists: [...this.state.optionLists],
+    //   });
+    // }, 1000);
+  };
+  onChange = (value, selectedOptions) => {
+    console.log(value, selectedOptions);
   };
   render() {
     const formItemLayout = {
@@ -56,7 +147,12 @@ export default class AddUpdate extends Component {
               />
             </Form.Item>
             <Form.Item label="商品分类" name="categoryId">
-              商品分类
+              <Cascader
+                options={this.state.optionLists}
+                loadData={this.loadData}
+                onChange={this.onChange}
+                changeOnSelect
+              />
             </Form.Item>
             <Form.Item label="商品图片" name="imgs">
               商品图片
