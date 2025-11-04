93008c8e91cce9cf6d6de5a5365fbf3fd3351cff
youngs <lixiang@popicorns.com>
2025-11-04 10:36:19 +0800

    feat:Redux DevTools Extension

--- admin_client/src/redux/store.js
+++ admin_client/src/redux/store.js
@@ -1,8 +1,13 @@
 import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "@redux-devtools/extension";
 
 import { countReducer } from "./reducer";
 
 import { thunk } from "redux-thunk";
-const store = createStore(countReducer, applyMiddleware(thunk));
+const store = createStore(
+  countReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+  //   applyMiddleware(thunk)
+);
 
 export default store;
